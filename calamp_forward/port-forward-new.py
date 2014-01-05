4e9a853708504aebfc8f54b2b92d806678d75a9e
Dylan Perales <dperales@gmail.com>
2014-01-05 00:55:04 -0800

    Modified port-forward-new.py

---
+++ calamp_forward/port-forward-new.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from socket import *
+bufsize = 1024 # Modify to suit your needs
+targetHost = "127.0.0.1"
+listenPort = 2500
+
+def forward(data, port):
+    print "Forwarding: '%s' from port %s" % (data, port)
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind(("localhost", port)) # Bind to the port data came in on
+    sock.sendto(data, (targetHost, listenPort))
+
+def listen(host, port):
+    listenSocket = socket(AF_INET, SOCK_DGRAM)
+    listenSocket.bind((host, port))
+    while True:
+        data, addr = listenSocket.recvfrom(bufsize)
+        forward(data, addr[1]) # data and port
+
+listen("localhost", listenPort)
