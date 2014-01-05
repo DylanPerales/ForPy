2044ff47d89221a16db6ae30c9738441b8cc5fd3
Dylan Perales <dperales@gmail.com>
2014-01-05 06:29:15 -0500

    Removed email address

--- forwardcheck.sh
+++ forwardcheck.sh
@@ -3,14 +3,18 @@ PATH=/usr/bin:/bin
 
 if [ "$(pidof python /root/forpy/port-forward.py)" ]
 then
- print "No Issues"
+  # NO ISSUES
+  true
 else
+  # Try to restart script
   python /root/forpy/port-forward.py &
   sleep 5
   if [ "$(pidof python /root/forpy/port-forward.py)" ]
   then
-    echo "ERROR: Process not running, Started Process Successfully!" |  mutt -s "ERROR: [TITAN] Port-Forward" dperales@gmail.com
+    # Script was started successfully
+    echo "ERROR: Process not running, Started Process Successfully!" |  mutt -s "ERROR: [TITAN] Port-Forward" email@domain.com # Edit with real Address
   else
-    echo "ERROR: Process not running, FAILED TO START PROCESS!" |  mutt -s "ERROR: [TITAN] Port-Forward" dperales@gmail.com
+    # Script did not start successfully
+    echo "ERROR: Process not running, FAILED TO START PROCESS!" |  mutt -s "ERROR: [TITAN] Port-Forward" email@domain.com # Edit with real Address
   fi
 fi
